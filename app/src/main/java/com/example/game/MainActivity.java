e3ebedd0a38b98a6f0817e415d538d90cad3babe
sanjith <sanjith1309@gmail.com>
2023-04-28 20:04:08 +0530

    a

--- app/src/main/java/com/example/game/MainActivity.java
+++ app/src/main/java/com/example/game/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     // 0== yellow 1== red 2== empty
+    public int[] gamestate={2,2,2,2,2,2,2,2,2};
+    public int[][] winningpositions={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,5,8},{2,5,7}};
     public int player = 0;
     public void dropin(View view){
         ImageView counter = (ImageView) view;
+        //at start the places should be empty
         counter.setTranslationY(-2000);
+
+        //array update
+        Log.i("tag-------",counter.getTag().toString());
+        int tag_of_place=Integer.parseInt(counter.getTag().toString());
+        gamestate[tag_of_place]=player;
+
+        // placing the coins
         if (player==0){
             counter.setImageResource(R.drawable.yellow);
             player=1;
